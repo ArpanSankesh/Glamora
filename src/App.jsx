36f7f9668180d1d16fd4b38d585798fe7aed7266
ArpanSankesh <arpansankesh830@gmail.com>
2025-07-31 13:20:25 +0530

    UI : Featured Services Section"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import NavBar from './Components/NavBar'
 import Hero from './Components/Hero'
+import FeaturedServices from './Components/FeaturedServices'
 
 const App = () => {
   return (
     <>
       <NavBar />
       <Hero />
-      <div className='w-full h-screen opacity-50'></div>
+      <FeaturedServices />
     </>
   )
 }
