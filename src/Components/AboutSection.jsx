32e2a2bf0d8c74e0dced26fac27f6c83299f8e58
ArpanSankesh <arpansankesh830@gmail.com>
2025-08-03 00:11:03 +0530

    UI : About Section complete

--- src/Components/AboutSection.jsx
+++ src/Components/AboutSection.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const AboutSection = () => {
   return (
-    <div className="w-full flex justify-center items-center px-6 md:px-16 lg:px-24 xl:px-32 py-20 relative">
-      <div className="w-full h-auto bg-[var(--color-accent)] rounded-3xl flex flex-col md:flex-row items-center justify-between px-10 py-12 md:py-20  overflow-hidden ">
+    <div className="w-full flex justify-center items-center px-6 md:px-16 lg:px-24 xl:px-32 py-20 ">
+      <div className="relative w-full h-auto bg-[var(--color-accent)] rounded-3xl flex flex-col md:flex-row items-center justify-between px-10 py-12 lg:py-20">
         
         {/* Text Content */}
         <div className="max-w-lg z-10 text-center md:text-left">
-          <h1 className="text-[var(--color-text)] text-4xl md:text-6xl font-bold leading-tight">
+          <h1 className="text-[var(--color-text)] text-4xl md:text-4xl xl:text-6xl font-bold leading-tight">
             Your Glow,<br /> Our Passion
           </h1>
-          <p className="text-[var(--color-text)] text-base md:text-lg leading-6 mt-3">
+          <p className="text-[var(--color-text)] text-base lg:text-lg leading-6 mt-3">
             More than a salon — it’s your beauty sanctuary.
           </p>
           <button className="px-8 py-3 bg-[var(--color-text)] text-[var(--color-accent)] font-bold rounded-xl mt-8">
@@ -19,11 +19,11 @@ const AboutSection = () => {
         </div>
 
         {/* Image */}
-        <div className="absolute bottom-21 right-0 w-full md:w-auto mt-10 md:mt-0 flex justify-center z-10">
+        <div className="hidden md:flex absolute bottom-0 right-[-50px] w-full md:w-auto mt-10 md:mt-0  justify-center z-15">
           <img
             src="./src/assets/3d.png" // Replace with your actual image path
             alt="About"
-            className="h-[620px] object-contain"
+            className="xl:h-[620px] lg:h-[500px] object-contain"
           />
         </div>
       </div>
