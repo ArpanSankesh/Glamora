46fd5ffdeb3437ea2dc33fb6b286514099249008
ArpanSankesh <arpansankesh830@gmail.com>
2025-08-03 00:19:00 +0530

    UI : Featured Service  Section complete

--- src/Components/FeaturedServices.jsx
+++ src/Components/FeaturedServices.jsx
@@ -1,22 +1,17 @@
 import React from 'react'
-// import Card from './Card'
-import Card2 from './Card2'
+import ProductCard from './ProductCard'
 
 const FeaturedServices = () => {
   return (
     <div className='my-20 mx-10 flex flex-col items-center justify-center'>
         <h1 className="text-3xl md:text-5xl font-medium text-[var(--color-accent)] text-center">Featured Services</h1>
-        <p className="text-[var(--color-secondary)] text-center md:text-xl">Explore our top beauty treatments.</p>
+        <p className="text-[var(--color-secondary)] text-center md:text-xl mt-2">Explore our top beauty treatments.</p>
         <div className="flex flex-wrap items-center justify-center gap-6 mt-20">
-            {/* <Card />
-            <Card />
-            <Card />
-            <Card /> */}
-            <Card2 />
-            <Card2 />
-            <Card2 />
-            <Card2 />
-            <Card2 />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
         </div>
         <button className='px-10 py-3 border-2 border-[var(--color-accent)] mt-20 rounded-full'>More</button>
     </div>
---
+++ src/Components/ProductCard.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const ProductCard = () => {
+  
+    const [count, setCount] = React.useState(0);
+    
+    
+        const product = {
+            name: "Casual Shoes",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cupiditate aperiam saepe quo aut soluta non dolor quisquam ducimus",
+            category: "Sports",
+            price: 100,
+            offerPrice: 80,
+            rating: 4,
+            image: "./src/assets/hair Spa.jpg", 
+        };
+    
+        return (
+            <div className="border border-[var(--color-secondary)] rounded-md  bg-white lg:min-w-56 lg:max-w-90 md:max-w-80 max-w-90 w-full">
+                <div className="group cursor-pointer flex items-center justify-center h-60 w-full bg-cover bg-center rounded-t-md"
+                    style={{ backgroundImage: `url(${product.image})` }}>
+    
+                </div>
+                <div className="p-5 text-gray-500/60 text-sm">
+                    <p className="text-[var(--color-secondary)] font-semibold text-xl truncate w-full">{product.name}</p>
+                    <p className="text-gray-700 font-medium text-sm  w-full">{product.description}</p>
+    
+                    <div className="flex items-end justify-between mt-3">
+                        <p className="md:text-xl text-base font-medium text-[var(--color-secondary)]">
+                            ${product.offerPrice} <span className="text-gray-500/60 md:text-sm text-xs line-through">${product.price}</span>
+                        </p>
+                        <div className="text-[var(--color-secondary)]">
+                            {count === 0 ? (
+                                <button className="flex items-center justify-center gap-1 bg-[var(--color-opaque)] border border-[var(--color-secondary)] md:w-[80px] w-[64px] h-[34px] rounded text-[var(--color-accent)] font-medium" onClick={() => setCount(1)} >
+                                    
+                                    Add
+                                </button>
+                            ) : (
+                                <div className="flex items-center justify-center gap-2 md:w-20 w-16 h-[34px] bg-[var(--color-text)] rounded select-none">
+                                    <button onClick={() => setCount((prev) => Math.max(prev - 1, 0))} className="cursor-pointer text-md px-2 h-full" >
+                                        -
+                                    </button>
+                                    <span className="w-5 text-center">{count}</span>
+                                    <button onClick={() => setCount((prev) => prev + 1)} className="cursor-pointer text-md px-2 h-full" >
+                                        +
+                                    </button>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+  )
+}
+
+export default ProductCard
