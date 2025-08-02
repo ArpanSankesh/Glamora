79fe7f963453326f4fa1ae25f05bf87321d13e2c
ArpanSankesh <arpansankesh830@gmail.com>
2025-08-02 23:01:23 +0530

    UI : Contack Section

--- src/Components/ContactSection.jsx
+++ src/Components/ContactSection.jsx
@@ -2,28 +2,27 @@ import React from 'react'
 
 const ContactSection = () => {
   return (
-     <form className="flex flex-col items-center text-sm py-20">
-            <p className="text-lg text-blue-600 font-medium pb-2">Contact Us</p>
-            <h1 className="text-4xl font-semibold text-slate-700 pb-4">Get in touch with us</h1>
-            <p className="text-sm text-gray-500 text-center pb-10">Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br />Lorem Ipsum has been the industry's standard dummy text.</p>
+     <form className="flex flex-col items-center justify-center text-sm py-20">
+            <p className="text-lg text-[var(--color-secondary)] font-medium md:pb-2">Contact Us</p>
+            <h1 className="md:text-4xl text-2xl font-semibold text-[var(--color-accent)] pb-4">Get in touch with us</h1>
             
-            <div className="flex flex-col md:flex-row items-center gap-8 w-[350px] md:w-[700px]">
+            <div className="flex flex-col md:flex-row items-center gap-8 w-[310px] md:w-[700px]">
                 <div className="w-full">
                     <label className="text-black/70" htmlFor="name">Your Name</label>
-                    <input className="h-12 p-2 mt-2 w-full border border-gray-500/30 rounded outline-none focus:border-indigo-300" type="text" required />
+                    <input className="h-12 p-2 mt-2 w-full border border-gray-500/30 rounded outline-none focus:border-[var(--color-secondary)]" type="text" required />
                 </div>
                 <div className="w-full">
                     <label className="text-black/70" htmlFor="name">Your Email</label>
-                    <input className="h-12 p-2 mt-2 w-full border border-gray-500/30 rounded outline-none focus:border-indigo-300" type="email" required />
+                    <input className="h-12 p-2 mt-2 w-full border border-gray-500/30 rounded outline-none focus:border-[var(--color-secondary)]" type="email" required />
                 </div>
             </div>
         
-            <div className="mt-6 w-[350px] md:w-[700px]">
+            <div className="mt-6 w-[310px] md:w-[700px]">
                 <label className="text-black/70" htmlFor="name">Message</label>
-                <textarea className="w-full mt-2 p-2 h-40 border border-gray-500/30 rounded resize-none outline-none focus:border-indigo-300" required></textarea>
+                <textarea className="w-full mt-2 p-2 h-40 border border-gray-500/30 rounded resize-none outline-none focus:border-[var(--color-secondary)]" required></textarea>
             </div>
         
-            <button type="submit" className="mt-5 bg-indigo-600 text-white h-12 w-56 px-4 rounded active:scale-95 transition">Send Message</button>
+            <button type="submit" className="mt-5 bg-[var(--color-accent)] text-white font-bold h-12 w-56 px-4 rounded-xl active:scale-95 transition">Send Message</button>
         </form>
   )
 }
