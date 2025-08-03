809a725a3cc7f8084654b65ada1f22ff5c978acb
ArpanSankesh <arpansankesh830@gmail.com>
2025-08-03 06:00:59 +0530

    feature : take data from file

--- src/Components/FeaturedServices.jsx
+++ src/Components/FeaturedServices.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import ProductCard from './ProductCard'
+import services from '../assets/data/servicesData'
 
 const FeaturedServices = () => {
+  
   return (
     <div className='my-20 mx-10 flex flex-col items-center justify-center'>
         <h1 className="text-3xl md:text-5xl font-medium text-[var(--color-accent)] text-center">Featured Services</h1>
         <p className="text-[var(--color-secondary)] text-center md:text-xl mt-2">Explore our top beauty treatments.</p>
         <div className="flex flex-wrap items-center justify-center gap-6 mt-20">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+          {services.map(service => (
+            <ProductCard
+            key={service.id}
+            name={service.name}
+            description={service.description}
+            image={service.image}
+            price={service.price}
+            offerPrice={service.offerPrice}
+            isAvailable={service.id}
+            />
+
+          ))}
         </div>
         <button className='px-10 py-3 border-2 border-[var(--color-accent)] mt-20 rounded-full'>More</button>
     </div>
--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,33 +1,21 @@
 import React from 'react'
 
-const ProductCard = () => {
+const ProductCard = ({ name, description, image, price, offerPrice }) => {
   
-    const [count, setCount] = React.useState(0);
-    
-    
-        const product = {
-            name: "Casual Shoes",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cupiditate aperiam saepe quo aut soluta non dolor quisquam ducimus",
-            category: "Sports",
-            price: 100,
-            offerPrice: 80,
-            rating: 4,
-            image: "./src/assets/hair Spa.jpg", 
-        };
-    
+    const [count, setCount] = React.useState(0);    
         return (
             <div className="border border-[var(--color-secondary)] rounded-md  bg-white lg:min-w-56 lg:max-w-90 md:max-w-80 max-w-90 w-full">
                 <div className="group cursor-pointer flex items-center justify-center h-60 w-full bg-cover bg-center rounded-t-md"
-                    style={{ backgroundImage: `url(${product.image})` }}>
+                    style={{ backgroundImage: `url(${image})` }}>
     
                 </div>
                 <div className="p-5 text-gray-500/60 text-sm">
-                    <p className="text-[var(--color-secondary)] font-semibold text-xl truncate w-full">{product.name}</p>
-                    <p className="text-gray-700 font-medium text-sm  w-full">{product.description}</p>
+                    <p className="text-[var(--color-secondary)] font-semibold text-xl truncate w-full">{name}</p>
+                    <p className="text-gray-700 font-medium text-sm  w-full">{description}</p>
     
                     <div className="flex items-end justify-between mt-3">
                         <p className="md:text-xl text-base font-medium text-[var(--color-secondary)]">
-                            ${product.offerPrice} <span className="text-gray-500/60 md:text-sm text-xs line-through">${product.price}</span>
+                            ₹{offerPrice} <span className="text-gray-500/60 md:text-sm text-xs line-through">${price}</span>
                         </p>
                         <div className="text-[var(--color-secondary)]">
                             {count === 0 ? (
